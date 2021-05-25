7a08dc3b32e59d9e027acf779e286083bd278e6b
RicardoMarassato <ricardo.marassato@gmail.com>
2021-05-24 21:38:57 -0300

    first commit

--- src/modules/users/repositories/implementations/UsersRepository.ts
+++ src/modules/users/repositories/implementations/UsersRepository.ts
@@ -18,24 +18,39 @@ class UsersRepository implements IUsersRepository {
     return UsersRepository.INSTANCE;
   }
 
-  create({ name, email }: ICreateUserDTO): User {
+  create({ name, email }: ICreateUserDTO): User{
     // Complete aqui
+    const user = new User();
+
+    Object.assign(user,{
+      name,
+      email,
+      created_at: new Date(),
+      updated_at: new Date(),
+      admin: false,
+    });
+
+    this.users.push(user);
+    return user;
   }
 
   findById(id: string): User | undefined {
     // Complete aqui
+    return null
   }
 
   findByEmail(email: string): User | undefined {
     // Complete aqui
+    return null
   }
 
   turnAdmin(receivedUser: User): User {
-    // Complete aqui
+    
+    return null
   }
 
   list(): User[] {
-    // Complete aqui
+    return this.users;
   }
 }
 
