362ede5f49a662c42aa04ec25a3e1224558a2a48
RicardoMarassato <ricardo.marassato@gmail.com>
2021-05-27 18:11:34 -0300

    Fixed jest tests

--- src/modules/users/useCases/listAllUsers/ListAllUsersController.ts
+++ src/modules/users/useCases/listAllUsers/ListAllUsersController.ts
@@ -3,16 +3,20 @@ import { Request, Response } from "express";
 import { ListAllUsersUseCase } from "./ListAllUsersUseCase";
 
 class ListAllUsersController {
-  constructor(private listAllUsersUseCase: ListAllUsersUseCase) {}
+  constructor(private listAllUsersUseCase: ListAllUsersUseCase) { }
 
   handle(request: Request, response: Response): Response {
     // Complete aqui
-    const {user_id} = request.body;
+    try {
+      const { user_id } = request.headers;
+      let users;
+      users = this.listAllUsersUseCase.execute({ user_id: String(user_id) });
+      return response.status(200).json(users);
+    } catch (error) {
+      return response.status(400).json({ error: error.message });
+    }
 
-    this.listAllUsersUseCase.execute({user_id});
-
-    return response.status(200).send();
   }
 }
 
-export { ListAllUsersController };
+export {ListAllUsersController}
--- src/modules/users/useCases/showUserProfile/ShowUserProfileController.ts
+++ src/modules/users/useCases/showUserProfile/ShowUserProfileController.ts
@@ -3,14 +3,18 @@ import { Request, Response } from "express";
 import { ShowUserProfileUseCase } from "./ShowUserProfileUseCase";
 
 class ShowUserProfileController {
-  constructor(private showUserProfileUseCase: ShowUserProfileUseCase) {}
+  constructor(private showUserProfileUseCase: ShowUserProfileUseCase) { }
 
   handle(request: Request, response: Response): Response {
     // Complete aqui
-    const {user_id} = request.body;
-    this.showUserProfileUseCase.execute({user_id});
+    try {
+      const { user_id } = request.params;
+      let user = this.showUserProfileUseCase.execute({ user_id });      
+      return response.status(201).json(user);
+    } catch (error) {
+      return response.status(404).json({error: error.message});
+    }
 
-    return response.status(200).send();
   }
 }
 
